5c5d2c8b67c5de0adf5fc4b62ddfc3050dbd6b03
Shane O'Brien <brienoshane93@gmail.com>
2021-10-18 10:01:18 +0100

    Version 2.0 Complete
    Implemented Placemark week 2 functionality

---
+++ Assignment1/src/main/kotlin/org/wit/assignment1/console/models/ExerciseModel.kt
@@ -0,0 +1,5 @@
+package org.wit.assignment1.console.models
+
+data class ExerciseModel(var id: Long = 0,
+                         var name: String = "",
+                         var set: String = "")
